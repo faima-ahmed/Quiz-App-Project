fc8a5e81718ebb3776f64d4cbbb91d78cd3be643
faima-ahmed <faimaahmeddiya@gmail.com>
2025-01-17 11:11:11 +0600

    quiz-app

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+const questions = [
+  {
+    questionText: "What is the capital of Bangladesh?",
+    answerOptions: [
+      { answerText: "New York", isCorrect: false },
+      { answerText: "London", isCorrect: false },
+      { answerText: "Dhaka", isCorrect: true },
+      { answerText: "Dublin", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "What is the capital of France?",
+    answerOptions: [
+      { answerText: "New York", isCorrect: false },
+      { answerText: "Paris", isCorrect: true },
+      { answerText: "London", isCorrect: false },
+      { answerText: "Dublin", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "What is the capital of India?",
+    answerOptions: [
+      { answerText: "New York", isCorrect: false },
+      { answerText: "London", isCorrect: false },
+      { answerText: "Paris", isCorrect: false },
+      { answerText: "Dehli", isCorrect: true },
+    ],
+  },
+  {
+    questionText: "What is the capital of Afghanistan?",
+    answerOptions: [
+      { answerText: "New York", isCorrect: false },
+      { answerText: "London", isCorrect: false },
+      { answerText: "Kabul", isCorrect: true },
+      { answerText: "Dublin", isCorrect: false },
+    ],
+  },
+  {
+    questionText: "What is the capital of Pakistan?",
+    answerOptions: [
+      { answerText: "New York", isCorrect: false },
+      { answerText: "Islamabad", isCorrect: true },
+      { answerText: "Paris", isCorrect: false },
+      { answerText: "Dublin", isCorrect: false },
+    ],
+  },
+];
+const App = () => {
+  return (
+    <div className="App">
+      <div>
+        <div>Quiz App</div>
+        <div>
+          <div>{questions[0].questionText}</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
